9df791da92f0fa6b024845989180f5535ea455bf
Jonathan Ngo <75549736+JonathanTNgo@users.noreply.github.com>
2025-03-31 16:29:34 -0500

    wrong pageable import

--- backend/src/main/java/com/ShapeStorefront/Shapes/ShapesRepository.java
+++ backend/src/main/java/com/ShapeStorefront/Shapes/ShapesRepository.java
@@ -1,7 +1,7 @@
 package com.ShapeStorefront.Shapes;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
