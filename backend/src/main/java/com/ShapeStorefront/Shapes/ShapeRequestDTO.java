02d923fb44342f56534dae5e7b5d0fd97dcc3844
Jonathan Ngo <75549736+JonathanTNgo@users.noreply.github.com>
2025-03-31 23:10:10 -0500

    Removed page / size info from requestDTO because Spring will handle pagination

--- backend/src/main/java/com/ShapeStorefront/Shapes/ShapeRequestDTO.java
+++ backend/src/main/java/com/ShapeStorefront/Shapes/ShapeRequestDTO.java
@@ -3,30 +3,18 @@ package com.ShapeStorefront.Shapes;
 import com.ShapeStorefront.Shapes.Shape_Enums.ShapeColor;
 import com.ShapeStorefront.Shapes.Shape_Enums.ShapeType;
 
-import jakarta.validation.constraints.*;
-
 public class ShapeRequestDTO {
     private String name;
     private ShapeColor color;
     private ShapeType type;
 
-    @Min(value = 1, message = "Elements per page must be greater than 0")
-    @Max(value = 50, message = "Elements per page must be less than 50")
-    private int elementsPerPage = 4;
-
-    @Min(value = 1, message = "Page number must be greater than 0")
-    // NOT 0 index based
-    private int pageNumber = 1;
-
     public ShapeRequestDTO() {
     }
 
-    public ShapeRequestDTO(String name, ShapeColor color, ShapeType type, int elementsPerPage, int pageNumber) {
+    public ShapeRequestDTO(String name, ShapeColor color, ShapeType type) {
         this.name = name;
         this.color = color;
         this.type = type;
-        this.elementsPerPage = elementsPerPage;
-        this.pageNumber = pageNumber;
     }
 
     public String getName() {
@@ -41,11 +29,4 @@ public class ShapeRequestDTO {
         return type;
     }
 
-    public int getElementsPerPage() {
-        return elementsPerPage;
-    }
-
-    public int getPageNumber() {
-        return pageNumber;
-    }
 }
